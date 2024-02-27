7661e02963ca213a1d35bf5e5819d2c1c683dc5c
Zehra Nur Olgun <zehra.olgun@i-2-m.eu>
2024-02-27 19:15:19 +0100

    feat: Add LLM integration

--- speechToText.py
+++ speechToText.py
@@ -1,6 +1,7 @@
 import pyaudio
 from google.cloud import speech
-from textToSpeech import text_to_speech
+from sdv_llm import get_and_speak_response
+
 # Create a speech client
 client = speech.SpeechClient()
 
@@ -8,8 +9,14 @@ client = speech.SpeechClient()
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
-# Create a stream
-def listen_print_loop(responses):
+
+def listen_print_loop(responses, keyword):
+    """Listens to microphone until the keyword is being heard
+
+    Args:
+        responses (StreamingRecognizeResponse): Response from stream recognizer
+        keyword (str): The keyword to trigger LLM
+    """
     for response in responses:
         if not response.results:
             continue
@@ -22,12 +29,19 @@ def listen_print_loop(responses):
         print(u'Transcript: {}'.format(result.alternatives[0].transcript))
         transcript = result.alternatives[0].transcript
         # Check if the keyword 'alpaca' is in the transcript
-        if 'alpaka' in transcript.lower():
+        if keyword in transcript.lower():
             print("Keyword 'alpaca' detected!")
-            # Here, trigger your desired action
-            text_to_speech("Yes, Zehra")
+            print(transcript)
+            transcript = transcript.replace("alpaka","")
+            get_and_speak_response(transcript)
+            # print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+            # transcript = result.alternatives[0].transcript
+            # continuous_chat_with_voice(transcript)
 
-def main():
+
+def speech_to_text():
+    """Listens the microphone continuously
+    """
     # Microphone stream setup
     audio_interface = pyaudio.PyAudio()
     audio_stream = audio_interface.open(format=pyaudio.paInt16,
@@ -51,7 +65,7 @@ def main():
     responses = client.streaming_recognize(streaming_config, stream)
     
     # Listen to the responses
-    listen_print_loop(responses)
+    listen_print_loop(responses, "alpaka")
 
 if __name__ == '__main__':
-    main()
+    speech_to_text()
