4e6461d624e0773c7c58636afa2618ab5947dc81
afri-bit <afri.bit@outlook.com>
2024-02-28 03:26:39 +0100

    Added skeleton to the integrated application

--- src/alpacai/app.py
+++ src/alpacai/app.py
@@ -1,4 +1,18 @@
+import argparse
+
+
+def run(args):
+    # TODO: Do things
+    ...
 
 
 if __name__ == "__main__":
-    print("")
+    parser = argparse.ArgumentParser(
+        prog="AttentiveAI",
+        description="Intelligent driver drowsiness avoidance with GenAI")
+    parser.add_argument("model_path", help="Path to the driver drowsiness path")
+    parser.add_argument("vehicle_ip", default="127.0.0.1", help="IP address to the vehicle interface communication")
+    parser.add_argument("vehicle_port", default="55555", help="Port communication to the vehicle")
+    args = parser.parse_args()
+
+    run(args)
