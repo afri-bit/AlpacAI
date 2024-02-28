f8d6bf2ba263c00e5e4a3408009e8e70719850e5
Zehra Nur Olgun <zehra.olgun@i-2-m.eu>
2024-02-28 11:28:24 +0100

    feat: add warning play functionality

--- src/alpacai/conversation_agent.py
+++ src/alpacai/conversation_agent.py
@@ -1,41 +1,42 @@
 import random
 import time
-
-# from speechToText import speech_to_text
 import googlemaps
 import pygame
 
 from alpacai.core.genai.sdv_llm import get_and_speak_response
 from alpacai.core.genai.text_to_speech import text_to_speech
 
+GOOGLE_API_KEY = ""
 
-def activate_ecal():
-    """_summary_
+
+def get_attentiveness_score():
+    """Generates dummy attentiveness score
 
     Returns:
-        _type_: _description_
+        int: Attentiveness score
     """
-    # @TODO: integrate eCal listener
-    return True
-
+    return random.randint(0, 100)
 
 def listen_to_car():
-    """_summary_
+    """Listens to values coming from car
     """
-    activate_ecal()
+    while True:
+        attentiveness_score = get_attentiveness_score()
+        print(attentiveness_score)
+        check_drowsiness(attentiveness_score)
 
 
-def play_music():
-    """_summary_
+def play_music(filepath):
+    """Play music given the file name
 
     Returns:
-        _type_: _description_
+        bool: Returns True if successful
     """
     # Initialize pygame mixer
     pygame.mixer.init()
 
-    # Load your MP3 file
-    pygame.mixer.music.load('electronic-rock-king-around-here-15045.mp3')
+    # Load MP3 file
+    pygame.mixer.music.load(filepath)
 
     # Play the MP3 file
     pygame.mixer.music.play()
@@ -50,9 +51,34 @@ def play_music():
 
     return True
 
+def play_warning(filepath):
+    """Plays warning sound 3 times
+
+    Returns:
+        bool: Returns True if successful
+    """
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Load MP3 file
+    pygame.mixer.music.load(filepath)
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Since play() is asynchronous, keep the script running until playback is done
+    while pygame.mixer.music.get_busy():
+        # Check the music playback status every 0.5 seconds
+        time.sleep(0.5)
+        # input('Press enter to continue...')
+        pygame.mixer.music.stop()  # Stop the music
+        break  # Exit the loop
+
+    return True
+
 
 def generate_conversation():
-    """_summary_
+    """Generate conversation with the driver given that they are getting sleepy
     """
     prompt_for_sleepy = "You are a car assistant and the driver is getting sleepy. \
                         What do you say to them? Just one sentence response"
@@ -61,38 +87,50 @@ def generate_conversation():
 
 
 def warn_driver():
-    """_summary_
-
-    Returns:
-        _type_: _description_
+    """Gives the driver haptic and sensual feedback
     """
     # @TODO: Steering vibration, fans
     return True
 
 
-def detect_drowsiness(drowsiness_level):
-    """_summary_
+def check_drowsiness(attentiveness_score):
+    """Checks the drowsiness level and takes action according to it
 
     Args:
-        drowsiness_level (_type_): _description_
+        attentiveness_score (int): Level of attentiveness in percentage(0-100)
     """
-    if 80 <= drowsiness_level <= 90:
-        text_to_speech("Let me turn on some music for you.")
-        play_music()
-    elif 50 <= drowsiness_level < 80:
-        generate_conversation()
-    else:
+
+    if 80 <= attentiveness_score <= 90:
+        prompt = "You are a car assistant and the driver is getting sleepy. \
+                You want to cheer them up and play some music for them. \
+                The music will start after you say your sentence. \
+                    Don't write [Music starts playing]"
+        # text_to_speech("Let me turn on some music for you.")
+        get_and_speak_response(prompt)
+        play_music('sounds/electronic-rock-king-around-here-15045.mp3')
+    elif 50 <= attentiveness_score < 80:
+        flag = random.randint(0,1)
+        if flag == 0:
+            generate_conversation()
+        else:
+            make_gps_suggestions(52.5170365, 13.3888599)
+    elif attentiveness_score < 50:
+        for i in range(3):
+            play_warning('sounds/warning.mp3')
+        prompt = "You are a car assistant and the driver is getting really really sleepy. \
+                You want to warn them firmly with a short message."
+        get_and_speak_response(prompt)
         warn_driver()
 
 
 def make_gps_suggestions(lat, lng):
-    """_summary_
+    """Make suggestions of coffee shops around the driver's GPS location
 
     Args:
-        lat (_type_): _description_
-        lng (_type_): _description_
+        lat (float): Latitude
+        lng (float): Longitude
     """
-    gmaps = googlemaps.Client(key='GOOGLE_API_KEY')
+    gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
 
     # Making a request to the Places API to find restaurants within 500 meters
     places_result = gmaps.places_nearby(location=(lat, lng), radius=500, type='cafe')
@@ -110,8 +148,9 @@ def make_gps_suggestions(lat, lng):
 if __name__ == "__main__":
     # Invoke speech detection
     # speech_to_text()
+    
+    check_drowsiness(85)
+    
+    # make_gps_suggestions(52.5170365, 13.3888599)
+
     # listen_to_car()
-
-    # detect_drowsiness(drowsiness_level=65)
-
-    make_gps_suggestions(52.5170365, 13.3888599)
